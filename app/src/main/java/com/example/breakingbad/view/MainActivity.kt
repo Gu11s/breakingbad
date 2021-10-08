b99afa4a8d587a5e437317c41d2d80aaa987ab12
GGarciaSalasZ <gsalas@grupozetagas.com>
2021-10-08 15:50:30 -0600

    Beginning the UI

--- app/src/main/java/com/example/breakingbad/view/MainActivity.kt
+++ app/src/main/java/com/example/breakingbad/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.breakingbad
+package com.example.breakingbad.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.breakingbad.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

47358f8371e96f14f515f8bfbc0c5d6dff5d9094
Gustavo García Salas <gggustavogs@gmail.com>
2021-10-10 09:13:27 -0600

    Creating the database

---
+++ app/src/main/java/com/example/breakingbad/model/CharacterDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.breakingbad.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = arrayOf(CharacterResponse::class), version = 1)
+abstract class CharacterDatabase: RoomDatabase() {
+
+    //using singleton to avoid many uses of the database at the same time
+    abstract fun characterDao(): CharacterDao
+
+    companion object {
+        @Volatile
+        private var instance: CharacterDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            CharacterDatabase::class.java,
+            "characterdatabase"
+        ).build()
+    }
+}
